d3b406279405bccbe19a10db81ef15def64b9518
Southboy <menkachi85@gmail.com>
2024-04-25 22:17:25 +0100

    Commented outSignup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Meme from './components/Meme'
+// import Signup from './components/Signup'
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Meme />
+            {/* <Signup /> */}
         </div>
     )
 }
